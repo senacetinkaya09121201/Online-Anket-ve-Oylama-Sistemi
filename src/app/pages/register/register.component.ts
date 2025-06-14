6516b850a0d8fb545c249eb437cacfa7b77ba365
Sena Cetinkaya <senacetnkayaas0912@gmail.com>
2025-06-14 22:27:32 +0300

    Register sayfası login ile uyumlu hale getirildi

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -5,7 +5,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  
+    styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
   email: string = '';
